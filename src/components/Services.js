a503d4a5e380096f0a66750fbdc4f359a8cfa78d
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-02-02 12:25:02 +0530

    aded

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,99 +1,30 @@
 import React from "react";
-import logo from "./download.png";
-import classes from './Service.module.css'
+
+import Card from "./Card";
+import Cards from "./Cards";
+import { motion, useScroll, useTransform } from "framer-motion";
+
 export default function Services() {
+    const {scrollY} =useScroll()
+  const opacities=useTransform(scrollY,[600,800,1000,1200],[1,0.6,0.3,0.1])
   return (
-    <div className="flex flex-col justify-center items-center px-3 md:flex">
-      <div className="">
+    <motion.div style={{opacity:opacities}} className="flex flex-col justify-center items-center">
+      
         <div className="text-center font-bold ">
           <h5 className="text-2 ">Services are</h5>
           <div className="text-[2rem] font-sans">
-            <p>SERVICE</p>
+            <p className="border-t-2 pt-0">SERVICE</p>
           </div>
         </div>
-        <div className={classes.img}>
-        <div className={`${classes.img1} flex justify-center px-[5rem] text-center gap-3 py-[2rem]  md:max-sm:flex-col`}>
-          <div className=" ">
-            <div>
-              <img src={logo} alt="images" width={40} height={30} />
-            </div>
-            <div>
-              <h2 className="font-bold">Communication</h2>
-              <div className="text-center">
-
-              <p className="">
-                this is service based company djjs ewrjwerwm wejrweiorwmwerwej
-              </p>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <img src={logo} alt="images" width={40} height={30}  />
-            </div>
-            <div>
-              <h2 className="font-bold">Communication</h2>
-              <p>
-                this is service based company djjs ewrjwerwm wejrweiorwmwerwej
-              </p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src={logo} alt="images" width={40} height={30}  />
-            </div>
-            <div>
-              <h2 className="font-bold">Communication</h2>
-              <p>
-                this is service based company djjs ewrjwerwm wejrweiorwmwerwej
-              </p>
-            </div>
-          </div>
-          </div>
-          <div className={`${classes.img1} flex justify-center px-[5rem] text-center gap-3 py-[2rem]`}> 
-          <div>
-            <div>
-              <img src={logo} alt="images" width={40} height={30} />
-            </div>
-            <div>
-              <h2 className="font-bold">Communication</h2>
-              <p>
-                this is service based company djjs ewrjwerwm wejrweiorwmwerwej
-              </p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src={logo} alt="images" width={40} height={30} />
-            </div>
-            <div>
-              <h2 className="font-bold">Communication</h2>
-              <p>
-                this is service based company djjs ewrjwerwm wejrweiorwmwerwej
-              </p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src={logo} alt="images" width={40} height={30} />
-            </div>
-            <div>
-              <h2 className="font-bold">Communication</h2>
-              <p>
-                this is service based company djjs ewrjwerwm wejrweiorwmwerwej
-              </p>
-            </div>
-          </div>
-        </div>
-        </div>
+ 
+      <Card/>
         <div className=" flex flex-col text-center bg-gray-300 px-[3rem] w-[100%]">
             <div className="gap-3">
-                <p className="text-sl text-black font-sans font-bold mb-2"> this is web application</p>
-                <button className="border-2 px-[3rem] text-white bg-black rounded-sm mb-[1rem]">Submit</button>
+                <p className="text-sl text-black font-sans font-bold mb-2 pt-3"> This is web application</p>
+                <button className="border-2 px-[3rem] hover:bg-orange-600 text-white bg-black rounded-md h-10 mb-[1rem]">Submit</button>
             </div>
         </div>
-      </div>
-    </div>
+   
+    </motion.div>
   );
 }
